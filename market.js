5a326ec5404c35ca9327de4de2fb026387dbedbe
monsite <lamraniamal161@gmail.com>
2024-08-14 14:07:14 +0100

    add a change

---
+++ market.js
@@ -0,0 +1,16 @@
+
+document.getElementById('home').addEventListener('click', function() {
+    window.location.href = 'market.html';
+});
+document.getElementById('login').addEventListener('click', function() {
+    window.location.href = 'login.html';
+});
+document.getElementById('search').addEventListener('click', function() {
+    window.location.href = 'search.html';
+});
+document.getElementById('bag').addEventListener('click', function() {
+    window.location.href = 'bag.html';
+});
+document.getElementById('service').addEventListener('click', function() {
+    window.location.href = '';
+});
