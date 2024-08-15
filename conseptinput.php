ea504b16ff81fd61bc384adf4d5b77de57dbba44
monsite <lamraniamal161@gmail.com>
2024-08-15 18:36:11 +0100

    add php

---
+++ conseptinput.php
@@ -0,0 +1,21 @@
+<?php
+$email = isset($_POST['email']) ? $_POST['email'] : '';
+$password = isset($_POST['password']) ? $_POST['password'] : '';
+
+
+$conn = new mysqli('localhost','root','','loginatria');
+if ($conn->connect_error) {
+    die('Connection Failed : ' . $conn->connect_error);
+} else {
+    $stmt = $conn->prepare("INSERT INTO atria (email, password) VALUES (?, ?)");
+
+    $stmt->bind_param("ss", $email, $password); 
+    $stmt->execute();
+    echo "Successful...";
+    $stmt->close();
+    $conn->close(); 
+}
+Header("Location: message.html");
+?>
+
+
